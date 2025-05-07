f34480f9105cabb962efa2c30b6f27f7b648c24a
Ansh <eye.am.ansh@gmail.com>
2025-05-07 00:36:39 -0700

    LEF, GDS, and LIB files generated!

---
+++ characterization/decks.eda-2.EECS.Berkeley.EDU.T20250507000114673637S3264862/strongARMLatch/delay_10/delay_10_001/skiVerboseRun.sh
@@ -0,0 +1,34 @@
+#!/bin/csh -f
+
+if !( -e bak ) then
+  echo "*Info*: Backup origional log files and waveforms"
+  mkdir -p bak
+else
+  rm -rf bak/*
+endif
+
+mv *.raw bak
+mv *.lis bak
+
+setenv SKI_STANDALONE_MODE 1
+
+
+# Turn on 'SKI_DEBUG_MSG' to show verbose messages.
+# setenv SKI_DEBUG_MSG 0x10000000
+
+# Use 'SKI_SAVE_WAVEFORMS' to save all waveforms of simulations.
+setenv SKI_SAVE_WAVEFORMS 1
+
+# Use 'SKI_DEBUG_NETLIST_DEP' to load verbose SKI netlist files.
+setenv SKI_DEBUG_NETLIST_DEP ski.dep
+
+setenv ALTOSHOME /share/instsww/cadence/LIBERATE
+
+set mmsim_path=/share/instsww/cadence/LIBERATE/tools.lnx86
+setenv LD_LIBRARY_PATH ${mmsim_path}/lib/64bit:${mmsim_path}/inca/lib/64bit:${mmsim_path}/mdl/lib/64bit:${mmsim_path}/mmsim/lib/64bit:${mmsim_path}/spectre/lib/64bit:${mmsim_path}/fmc/lib/64bit:${mmsim_path}/dfII/lib/64bit:${LD_LIBRARY_PATH}
+
+
+spectre -64 -plugin /share/instsww/cadence/LIBERATE/lib/lnx86_64_2.6/libSki.so +multiple -cp -ss +l sim.lis -f sst2 +lqt 0 
+
+unsetenv SKI_STANDALONE_MODE
+rm -rf stdin.raw
